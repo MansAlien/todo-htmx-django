eab6ab7c8a8ade8c782d80ac8e849d74717fb7b8
MansAlien <mans012033945421@gmail.com>
2024-03-20 07:31:14 +0200

    CRUD with htmx & django

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from todo.views import TodoTemplateView, TodoListView
+from todo.views import TodoListView, createview, renameview, cancelview, applyview, deleteview
 
 urlpatterns = [
-    path("", TodoTemplateView.as_view(), name="home" ),
-    path("list/", TodoListView.as_view(), name="list"),
+    path("", TodoListView.as_view(), name="home"),
+    path("create/", createview, name="create"),
+    path("<int:pk>/rename/", renameview, name="rename"),
+    path("cancel/", cancelview, name="cancel"),
+    path("<int:pk>/apply/", applyview, name="apply"),
+    path("<int:pk>/delete/", deleteview, name="delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,43 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView
+from django.shortcuts import render
 from todo.models import Task
-
-class TodoTemplateView(TemplateView):
-    model = Task
-    template_name = "home.html"
-
+from django.http import HttpResponse
 
 class TodoListView(ListView):
     model = Task
     template_name = "home.html"
+
+def createview(request):
+    task_name = request.POST.get("task")
+    task = Task.objects.create(title=task_name)
+    task_list = Task.objects.all()
+    context = {"task_list":task_list}
+    return render(request, "home.html", context)
+
+def cancelview(request):
+    task_list = Task.objects.all()
+    context = {"task_list":task_list}
+    return render(request, "home.html", context)
+
+def renameview(request, pk):
+    task = Task.objects.get(id=pk)
+    context = {"task":task}
+    return render(request, "edit.html", context)
+
+
+
+def applyview(request, pk):
+    task = Task.objects.get(id=pk)
+    task.title = request.POST.get("update") 
+    task.save()
+    task_list = Task.objects.all()
+    context = {"task_list":task_list}
+    return render(request, "home.html", context) 
+
+def deleteview(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    task_list = Task.objects.all()
+    context = {"task_list":task_list}
+    return render(request, "home.html", context)
+
